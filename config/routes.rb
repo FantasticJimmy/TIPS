27698f915dd1ccef69199eb82dc292fe26a46554
Jimmy Fu <dboyjimmy@gmail.com>
2016-06-04 12:26:46 -0400

    testing first post API

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   post '/estimate' => 'estimate#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
+  root to: 'home#index'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
